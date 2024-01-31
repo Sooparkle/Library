c759e14f38e1223312f5a6a048ea1e3488067c74
Sooparkle <imsoohanmu@gmail.com>
2024-01-31 18:12:30 +0900

    developing SearchArea through a component

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import {BrowserRouter, Link, Route} from 'React-Router-dom';
 import Youtube from './component/Youtube';
 import logo from './logo.svg';
 import BookList from './component/BookList';
+import SearchArea from './component/SearchArea';
 
 
 function App() {
 
-  const [title, setTitle] = useState('서울시');
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [formResult, setFormResult] = useState("");
-  const [showButton, setShowButton] = useState(false);
+  const [ guLibrary, setGuLibrary ] = useState([]);
+  // const [title, setTitle] = useState('서울시');
+  // const [searchKeyword, setSearchKeyword] = useState("");
+  // const [formResult, setFormResult] = useState("");
+  // const [showButton, setShowButton] = useState(false);
   const [scrollY, setScrollY] = useState(0);
   const [count, setCount] = useState(0);
-  const [newLibrary, setNewLibrary] = useState([]);
+  // const [newLibrary, setNewLibrary] = useState([]);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] =useState();
@@ -23,33 +25,33 @@ function App() {
   const start = useRef();
   const end = useRef();
 
-  const library =[
-    {id : 1, value : 11010 , libName : "종로구" },
-    {id : 2, value : 11020, libName : "중구" },
-    {id : 3, value : 11030, libName : "용산구" },
-    {id : 4, value : 11040, libName : "성동구" },
-    {id : 5, value : 11050, libName : "광진구" },
-    {id : 6, value : 11060, libName : "동대문구" },
-    {id : 7, value : 11070, libName : "중랑구" },
-    {id : 8, value : 11080, libName : "성북구" },
-    {id : 9, value : 11090, libName : "강북구" },
-    {id : 10, value : 11100, libName : "도봉구" },
-    {id : 11, value : 11110, libName : "노원구" },
-    {id : 12, value : 11120, libName : "은평구" },
-    {id : 13, value : 11130, libName : "서대문구" },
-    {id : 14, value : 11140, libName : "마포구" },
-    {id : 15, value : 11150, libName : "양천구" },
-    {id : 16, value : 11160, libName : "강서구" },
-    {id : 17, value : 11170, libName : "구로구" },
-    {id : 18, value : 11180, libName : "금천구" },
-    {id : 19, value : 11190, libName : "영등포구" },
-    {id : 20, value : 11200, libName : "동작구" },
-    {id : 21, value : 11210, libName : "관악구" },
-    {id : 22, value : 11220, libName : "서초구" },
-    {id : 23, value : 11230, libName : "강남구" },
-    {id : 24, value : 11240, libName : "송파구" },
-    {id : 25, value : 11250, libName : "강동구" },
-  ]
+  // const library =[
+  //   {id : 1, value : 11010 , libName : "종로구" },
+  //   {id : 2, value : 11020, libName : "중구" },
+  //   {id : 3, value : 11030, libName : "용산구" },
+  //   {id : 4, value : 11040, libName : "성동구" },
+  //   {id : 5, value : 11050, libName : "광진구" },
+  //   {id : 6, value : 11060, libName : "동대문구" },
+  //   {id : 7, value : 11070, libName : "중랑구" },
+  //   {id : 8, value : 11080, libName : "성북구" },
+  //   {id : 9, value : 11090, libName : "강북구" },
+  //   {id : 10, value : 11100, libName : "도봉구" },
+  //   {id : 11, value : 11110, libName : "노원구" },
+  //   {id : 12, value : 11120, libName : "은평구" },
+  //   {id : 13, value : 11130, libName : "서대문구" },
+  //   {id : 14, value : 11140, libName : "마포구" },
+  //   {id : 15, value : 11150, libName : "양천구" },
+  //   {id : 16, value : 11160, libName : "강서구" },
+  //   {id : 17, value : 11170, libName : "구로구" },
+  //   {id : 18, value : 11180, libName : "금천구" },
+  //   {id : 19, value : 11190, libName : "영등포구" },
+  //   {id : 20, value : 11200, libName : "동작구" },
+  //   {id : 21, value : 11210, libName : "관악구" },
+  //   {id : 22, value : 11220, libName : "서초구" },
+  //   {id : 23, value : 11230, libName : "강남구" },
+  //   {id : 24, value : 11240, libName : "송파구" },
+  //   {id : 25, value : 11250, libName : "강동구" },
+  // ]
 
 // 슬라이더
 // https://www.youtube.com/watch?v=iyj3TZXg2gQ
@@ -58,45 +60,45 @@ function App() {
   //https://velog.io/@jahommer/React-Top%EB%B2%84%ED%8A%BC-scroll-to-top%EB%A7%8C%EB%93%A4%EA%B8%B0
 
 //input 정보 변경 및 저장하기
-  const handleChangeInput = (e)=>{
-    const inputKeyValue = e.target.value;
-    setSearchKeyword(inputKeyValue);
-  };
+//   const handleChangeInput = (e)=>{
+//     const inputKeyValue = e.target.value;
+//     setSearchKeyword(inputKeyValue);
+//   };
 
-// form 으로 검색 값 submit 하기 
-  const handleResult = (e)=>{
-    e.preventDefault();
-    setFormResult(searchKeyword);
-    searchLibDatas(searchKeyword);
-    console.log(searchKeyword);
-  };
+// // form 으로 검색 값 submit 하기 
+//   const handleResult = (e)=>{
+//     e.preventDefault();
+//     setFormResult(searchKeyword);
+//     searchLibDatas(searchKeyword);
+//     console.log(searchKeyword);
+//   };
 
 // Library API 정보  fetch로 data 가져오기 
-  const searchLibDatas = (keyword)=>{
-    for ( var i= 0 ; i < library.length; i++){
-      if(keyword == library[i].libName){
-        setTitle(library[i].libName)
-        const selectedLib = library[i].value
-        const url = `http://data4library.kr/api/loanItemSrchByLib?authKey=43d7efdc5d7f99a3be907ecac62d3212026fb810e793f19e56fb0b5a390c93f8&dtl_region=${selectedLib}&startDt=2023-01-01&endDt=2023-12-31&format=json`
+  // const searchLibDatas = (keyword)=>{
+  //   for ( var i= 0 ; i < library.length; i++){
+  //     if(keyword == library[i].libName){
+  //       setTitle(library[i].libName)
+  //       const selectedLib = library[i].value
+  //       const url = `http://data4library.kr/api/loanItemSrchByLib?authKey=43d7efdc5d7f99a3be907ecac62d3212026fb810e793f19e56fb0b5a390c93f8&dtl_region=${selectedLib}&startDt=2023-01-01&endDt=2023-12-31&format=json`
         
-          fetch(url).then((response)=>{
-            if(response.status !== 200){
-              console.log("서버 통신 실패!");
-            } else {
-              return response.json();
-            }
-          }).then((data) => {
-            const jsonData = data.response.docs
-            setNewLibrary(jsonData);
-            console.log(jsonData);
-          })
+  //         fetch(url).then((response)=>{
+  //           if(response.status !== 200){
+  //             console.log("서버 통신 실패!");
+  //           } else {
+  //             return response.json();
+  //           }
+  //         }).then((data) => {
+  //           const jsonData = data.response.docs
+  //           setNewLibrary(jsonData);
+  //           console.log(jsonData);
+  //         })
 
-      }
-    }
-    };
+  //     }
+  //   }
+  //   };
 
 
-    console.log
+
 // scroll Top button 
     const handleScroll = () =>{
       window.scroll({top:0, behavior:'smooth'});
@@ -104,26 +106,12 @@ function App() {
 
   return (
     <>
-      <div className="search-background">
-        <h1><span className="lib-title">{title}</span> 우리 동네 도서관 <br/>대출 베스트 200</h1>
-        <div className="search-wrap">
-          <div className="logo-wrap">
-            <img src={logo} alt="logo" />
-          </div>
-          
-          <form onSubmit={handleResult}>
-            <div className="input-wrap">
-              <input type="text" value={searchKeyword} onChange={handleChangeInput} placeholder="'-구'를 입력해주세요! 예시)종로구"></input>
-            </div>
-          </form>
-          <button type="submit">검색</button>
-        </div>
-      </div>
+      <SearchArea setGuLibrary={setGuLibrary}/>
       <Youtube/>
 
       <div className="data-wrap">
         {
-        newLibrary.map((item) => { 
+        guLibrary.map((item) => { 
           return(
             <BookList 
               key={item.doc.no}
--- src/component/Youtube.tsx
+++ src/component/Youtube.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import react from "@vitejs/plugin-react-swc"
-import App from './App'
+
 
 
 const Youtube =() => {
